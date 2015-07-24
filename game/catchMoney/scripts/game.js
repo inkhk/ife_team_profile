29a53fc98d08a6b402e230e729169a5bb4099c41
Tony <wt911122@hotmail.com>
2015-07-25 01:38:47 +0800

    完善了计时模块和游戏结束事件
    @inkhk 请借助scene.js与game.js中的game.initGameOver中的事件处理函数，完成游戏结束的弹出窗口

--- game/catchMoney/scripts/game.js
+++ game/catchMoney/scripts/game.js
@@ -10,43 +10,54 @@ var createjs = createjs || {};
 		ticksPerNewDiamond: 20,
 		fallingSpeed: 5,
 		diamondWidth: 90,
+		gameTimeout: 2
 	}
 }).call(this, game);
 
 //Tick
-;(function(){
+;(function(cjs){
 	game.Tick = function(e){
 		game.stage.update();
 
 		if (!e.paused) {
 			game.gameView.moveObjects();
+			if(game.gameView.diamondFactoryState){
+				game.gameView.timer.showTime(cjs.Ticker.getTime());
+			}
+
 			var ticksCount = createjs.Ticker.getTicks(true);
-			if (ticksCount % game.setting.ticksPerNewDiamond === 0) {
+			if (game.gameView.diamondFactoryState && ticksCount % game.setting.ticksPerNewDiamond === 0) {
 				game.gameView.generateDiamond();
 			};
+			game.gameView.checkOnDiamonds();
 		};
 	}
-}).call(this);
+}).call(this, createjs);
 
 ;(function(game, cjs){
 	//TODO游戏逻辑
 	game.gameView = {
 		countBoard: {},
 		diamondsRepositories: [],
+		diamondFactoryState: true,
 		generateDiamond: function(){
-			//console.log('generateDiamond');
 			diamond = new game.diamond();
 			diamond.y = - game.setting.diamondWidth;
 			diamond.x = Math.random() * (game.setting.gameWidth - game.setting.diamondWidth);
-		//	console.log(diamond);
 			game.stage.addChild(diamond);
 			this.diamondsRepositories.push(diamond);
 		},
 		init: function(){
 			this.putOnCountBoard();
+			this.putOnTimerBoard();
 			this.generateDiamond();
-			createjs.Ticker.setFPS(40);
-			createjs.Ticker.addEventListener('tick', game.Tick);
+			cjs.Ticker.setFPS(40);
+			cjs.Ticker.addEventListener('tick', game.Tick);
+		},
+		reset: function(){
+			game.stage.removeAllChildren();
+			this.diamondFactoryState = true;
+			this.init();
 		},
 		moveObjects: function(){
 			for(var i=0, len=this.diamondsRepositories.length; i<len; i++){
@@ -69,9 +80,18 @@ var createjs = createjs || {};
 				};
 			}
 		},
+		checkOnDiamonds: function(){
+			if (this.diamondsRepositories.length === 0) {
+				game.stage.dispatchEvent(new game.helper.gameOverEvent(10));
+			};
+		},
 		putOnCountBoard: function(){
 			this.countBoard = new game.diamondBoard();
-			game.stage.addChildAt(this.countBoard);
+			game.stage.addChild(this.countBoard);
+		},
+		putOnTimerBoard: function(){
+			this.timer = new game.clock();
+			game.stage.addChild(this.timer);
 		}
 	}
 
@@ -80,12 +100,26 @@ var createjs = createjs || {};
 			{src: "cat_01.png", id: "cat"},
 			{src: "money.png", id: "money"},
 			{src: "diamond-spritesheet.png", id: "diamond"},
-			{src: "diamonds.png", id:"diamondsBoard"}
+			{src: "diamonds.png", id:"diamondsBoard"},
+			{src: "clock.png", id: "clock"}
 		];
 		game.loader = new cjs.LoadQueue(false);
 		game.loader.addEventListener("complete", complete);
 		game.loader.loadManifest(manifest, true, "imgs/");
 
+	}
+
+	game.initGameOver = function(){
+		game.stage.addEventListener('gameOverEvent', function(e){
+			cjs.Ticker.removeEventListener('tick', game.Tick);
+			console.log(e);
+		})
+	}
+	game.initStopGenerateDiamond = function(){
+		game.stage.addEventListener('stopGenerateDiamondEvent', function(e){
+			console.log('stop');
+			game.gameView.diamondFactoryState = false;
+		})
 	}
 }).call(this, game, createjs);
 
@@ -104,6 +138,8 @@ var createjs = createjs || {};
 			//game.stage.addChild(diamond);
 
 			//game.
+			game.initGameOver();
+			game.initStopGenerateDiamond();
 			game.gameView.init();
 		}
 		game.initLoader(handleComplete);
--- game/catchMoney/scripts/view-sprites.js
+++ game/catchMoney/scripts/view-sprites.js
@@ -31,27 +31,81 @@ var createjs = createjs || {};
 		var background =  new cjs.Bitmap(game.loader.getResult("diamondsBoard"));
 		background.x = 0;
 		background.y = 0;
-		var text = new createjs.Text("0", "20px Impact", "#000");
+		var text = new cjs.Text("0", "20px Impact", "#000");
 		text.textBaseline = 'middle';
 		text.x = background.getBounds().width / 2 + 25;
 		text.y = background.getBounds().height / 2;
+		console.log(text.x, text.y);
 		text.textAlign = 'center';
 		this.addChild(background);
 		this.addChild(text);
 		this.x = 10;
 		this.y = 10;
 
-		this.addNumberText = function(num){
+		this.addNumberText = function(){
 			text.text = parseInt(text.text) + 1;
+		}
+		this.getNumber = function(){
+			return text.text;
+		}
+	}).prototype = Object.create(cjs.Container.prototype);
+
+	(game.clock = function(){
+		cjs.Container.call(this);
+		var clockPic = new cjs.Bitmap(game.loader.getResult('clock'));
+		clockPic.x = 50;
+		clockPic.y = 10;
+		var text = new cjs.Text('00:00', "20px Impact", "#000");
+		text.textBaseline = 'middle';
+		text.x = 0;
+		text.y = 24.5;
+
+		this.addChild(clockPic);
+		this.addChild(text);
+		this.x = game.setting.gameWidth - 80;
+		this.y = 10;
+		
+		this.showTime = function(timeSpan){
+			var now = new Date(timeSpan),
+				str = '',
+				delta = 1000/60
+
+			var second = now.getSeconds();
+			if (Math.floor(second / 10) === 0) {
+				str += '0';
+				str += second;
+			}else{
+				str += second;
+			}
+			str += ':'
+
+			if (second === game.setting.gameTimeout) {
+				console.log('timeout')
+				this.dispatchEvent(new game.helper.stopGenerateDiamondEvent());
+				str += '00';
+				text.text = str;
+				return;
+			};
+
+			var millionSecond = Math.floor(now.getMilliseconds() / delta)
+			if (Math.floor(millionSecond / 10) === 0) {
+				str += '0';
+				str += millionSecond;
+			}else{
+				str += millionSecond;
+			}
+			text.text = str;
+
 		}
 	}).prototype = Object.create(cjs.Container.prototype);
 
 	(game.cat = function(){
 		cjs.Bitmap.call(this, game.loader.getResult("cat"));
-		this.x = 0;
+		//this.x = game.setting.gameWidth/2-this.getBounds().width*this.scaleX/2;
 		this.y = 0;
 		this.scaleX = 0.5;
 		this.scaleY = 0.5;
+		this.x = game.setting.gameWidth/2-this.getBounds().width*this.scaleX/2;
 	}).prototype = Object.create(cjs.Bitmap.prototype);
 
 }).call(this, game, createjs)
