987a8d516ff029e99fba345075abac58eda66446
Lauren Song <inkhksong@gmail.com>
2015-07-26 00:39:25 +0800

    handle game start scene

--- game/catchMoney/scripts/scenes.js
+++ game/catchMoney/scripts/scenes.js
@@ -1,25 +1,10 @@
 var game = this.game || (this.game = {});
-
-;(function(){
-	var scene = game.scene = {
-		node: document.querySelector('.scene'),
-		setup: function(){},
-		onShow: function(){},
-		show: function(){
-			this.node.classList.remove('out');
-			this.node.classList.add('in');
-			this.onShow();
-		},
-		hide: function(){
-			this.node.classList.remove('in');
-			this.node.classList.add('out');
-		}
-	}
-
-	var startScene = game.startScene = Object.create(scene);
-	//TODO
-	
-
-	var gameOverScene = game.gameOverScene = Object.create(scene);
-	//TODO
-})
+//对不起我把原有的代码换成了小白版本...by inkhk
+     window.onload = function() {
+            var button = document.getElementById('start-btn');
+            button.onclick = function(e) {
+                document.getElementById('gamebg').style.display = 'none';
+                document.getElementById('canvas').style.display = 'block';
+                game.start();
+            }
+        }
